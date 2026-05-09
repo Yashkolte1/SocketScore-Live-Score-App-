a9045e84d5bd188b7dba2fe30cbcc108aca8e4b7
Yashkolte1 <yashthetopslugger@gmail.com>
2026-05-09 10:44:38 +0530

    Implemented Matches Get And Post Routes And Checked Those Using HTTPIE Also Added Zod Validation To The Database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {matchRouter} from './routes/matches.js';
 
 const app = express();
 const port =  8000;
@@ -7,8 +8,10 @@ const port =  8000;
 app.use(express.json());
 
 app.get('/' , (req, res) => {
-    res.send('Hello From Server!');
+    res.send('Hello From Server Side!');
 });
+
+app.use('/matches' , matchRouter)
 
 app.listen(port , () => {
     console.log(`Server Running AT http://localhost:${port}`);
